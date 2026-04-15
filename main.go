133700d3a660f8e1663c865d77b4ca3b5138fe51
Ryan Munro <500774+munro@users.noreply.github.com>
2026-04-14 21:17:51 -0400

    bump version to 0.1.2

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"syscall"
 )
 
-const version = "0.1.1"
+const version = "0.1.2"
 
 func usage() {
 	fmt.Fprintf(os.Stderr, `gorun %s — compile-on-change runner for single-file Go programs
